219690112a94a4b3096c56244cc63cfe054a5998
Kenny K <koablood@gmail.com>
2023-01-13 18:16:44 -0800

    with_router

---
+++ src/components/AlbumView.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+const AlbumView = (props) => {
+    const { id } = useParams()
+    const navigate = useNavigate()
+    const [albumData, setAlbumData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/song/${id}`
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => navigate(-1)}>back</button>
+                |
+                <button onClick={() => navigate('/')}>home</button>
+            </div>
+        )
+    }
+
+    const allAlbums = albumData.filter(entity => entity.kind === 'song')
+        .map((album, i) => { return (<div key={i}>{album.trackName}</div>) })
+
+    return (
+        <div>
+            {albumData.length > 0 ? <h2>{albumData[0].collectionName}</h2> : <p>loading...</p>}
+            {navButtons()}
+            {allAlbums}
+        </div>
+    )
+}
+
+export default AlbumView
