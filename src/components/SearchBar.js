080d5007fe2abd6e636fec9da5ea43ac48432ba8
Kenny K <koablood@gmail.com>
2023-01-13 22:29:03 -0800

    redue

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,15 +3,16 @@ import { useContext } from 'react'
 import { SearchContext } from '../context/SearchContext'
 
 function SearchBar() {
-    let [searchTerm, setSearchTerm] = useState('')
+    let [searchTerm, setSearchTerm] = useState('null')
     const { term, handleSearch } = useContext(SearchContext)
 
     return (
-        <form onSubmit={(e) => handleSearch(e, searchTerm.current.value)}>
+        <form onSubmit={(e) => handleSearch(e, term.current.value)}>
 
             <input type="text" placeholder="Enter a search term here" ref={term} onChange={(e) => setSearchTerm(e.target.value)}/>
             <input type="submit"/>
-            </form>
+
+        </form>
     )
 }
 
