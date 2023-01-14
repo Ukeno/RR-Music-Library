96d8ca10f9783bc67157d9e065e0bb8b3f0f46c1
Kenny K <koablood@gmail.com>
2023-01-13 21:16:05 -0800

    React_dom_context

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import { useContext } from 'react'
+import { SearchContext } from '../context/SearchContext'
 
-function SearchBar(props){
+function SearchBar() {
     let [searchTerm, setSearchTerm] = useState('')
+    const { term, handleSearch } = useContext(SearchContext)
 
     return (
-            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-                <input type="text" placeholder="Search Here" onChange={(e) => setSearchTerm(e.target.value)} />
-                <input type="submit" />
+        <form onSubmit={(e) => handleSearch(e, searchTerm.current.value)}>
+
+            <input type="text" placeholder="Enter a search term here" ref={term} onChange={(e) => setSearchTerm(e.target.value)}/>
+            <input type="submit"/>
             </form>
     )
 }
